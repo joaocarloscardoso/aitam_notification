c903126727812dcf1a516c00a4e7827f831b5e36
Joao Cardoso <joao.ccardoso@gmail.com>
2020-03-28 17:13:52 +0000

    Notifications added 20200328

--- index.js
+++ index.js
@@ -1,81 +1,31 @@
-const axios = require('axios');
+var app = require('express')();
+var http = require('http').Server(app);
 
-//logging system
-var db = require('./lib/db.js');
+var io = require('socket.io')(http);
 
-//data to be sent and/or stored in notification mechanism
-var SessionId=Math.floor(Date.now() / 1001).toString();
-var ObjectID='rec20200102';
-var Subject='Question on recommendation';
+var service = require('./lib/service.js');
 
-function CreateRequest(SessionId, ObjectID, Subject){
-    //get request through axios component
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-        .then(response => {
-        
-            //to be obtained by the service response
-            var RequestId=Math.floor(Date.now() / 1000).toString();
-            
-            db.CreateRequestCUBE(SessionId, RequestId, ObjectID, Subject);
-            //console.log(response.data.url);
-            //console.log(response.data.explanation);
-        })
-        .catch(error => {
-            console.log(error);
-    });
-};
 /*
-// multiple concurrent requests with axios
-axios.all([
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-03-26'),
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-03-25')
-    ]).then(axios.spread((response1, response2) => {
-        console.log(response1.data.url);
-        console.log(response2.data.url);
-    })).catch(error => {
-        console.log(error);
-    });
-*/
-//post request through axios component
-/*
-axios({
-    "method": "POST",
-    "url": "http://httpbin.org/post",
-    "params": {
-        "key": "abc123"
-    },
-    "data": {
-        "firstname": "Nic",
-        "lastname": "Raboy"
-    }
-}).then(response => {
-    console.log(response.data);
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
 });
 */
 
-db.DeleteByRequestID('DEMO_KEY');  
-db.DeleteBySessionID('1583652701');
+http.listen(3010, function() {
+    console.log('server listening on port: 3010');
+    io.on("connection", socket => {
+        console.log("New client connected: " + socket.id);
 
-function CheckData(requestID){
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then(response => {
-        if (response.data.url != '' || response.data.explanation !='') {
-            db.UpdateDataCUBE(requestID, response.data.url,response.data.explanation)
-            //later:create notification for sessionid ;
-        }
-    })
-    .catch(error => {
-        console.log(error);
+        socket.on("requestCUBE", function(message){
+            service.CreateRequest(message.sessionid, message.objectid, message.subject);
+            console.log(message);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
     });
-};
+});
 
-function ListenerOpenRequests(){
-    db.GetOpenRequests().then(function(Requests){
-        for (var i=0; i<Requests.length; i++) {
-            CheckData(Requests[i].requestid);
-        };
-    });
-};
 
-//CreateRequest(SessionId, ObjectID, Subject);
-ListenerOpenRequests();
+
